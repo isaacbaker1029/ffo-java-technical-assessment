f1bad7bf54299b0bbb598d85ba3066070a8b5d86
Isaac <isaac.l.baker@hotmail.com>
2025-08-04 18:22:12 +0100

    chore: converted to springboot + fixed package spelling

---
+++ src/main/java/com/ffo/assessment/JobNormaliserApplication.java
@@ -0,0 +1,13 @@
+package com.ffo.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobNormaliserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobNormaliserApplication.class, args);
+    }
+
+}
