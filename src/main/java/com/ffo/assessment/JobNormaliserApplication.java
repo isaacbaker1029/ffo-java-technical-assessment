a622d15c7523964a4ba9b5d39587e9b39d8fb7ec
Isaac <isaac.l.baker@hotmail.com>
2025-08-04 18:46:13 +0100

    feat: added job titles into config and loaded into service

--- src/main/java/com/ffo/assessment/JobNormaliserApplication.java
+++ src/main/java/com/ffo/assessment/JobNormaliserApplication.java
@@ -1,13 +1,32 @@
 package com.ffo.assessment;
 
+import com.ffo.assessment.service.NormalisationService;
+import com.ffo.assessment.service.impl.SimpleNormaliser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JobNormaliserApplication {
+public class JobNormaliserApplication implements CommandLineRunner {
+
+    @Autowired
+    NormalisationService normalisationService;
 
     public static void main(String[] args) {
         SpringApplication.run(JobNormaliserApplication.class, args);
+    }
+
+    // This run method will be executed automatically after the application starts
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("\n--- RUNNING NORMALISATION TEST ---");
+
+        // Let's call your method with a test input
+        String result = normalisationService.normalise("Java engineer");
+
+        System.out.println("\nNormalisation result: '" + result + "'");
+        System.out.println("--- TEST COMPLETE ---\n");
     }
 
 }
--- src/main/java/com/ffo/assessment/service/impl/SimpleNormaliser.java
+++ src/main/java/com/ffo/assessment/service/impl/SimpleNormaliser.java
@@ -1,12 +1,25 @@
 package com.ffo.assessment.service.impl;
 
 import com.ffo.assessment.service.NormalisationService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SimpleNormaliser implements NormalisationService {
+
+    private final List<String> normalisedTitles;
+
+    // Spring injects the value from application.properties into this constructor
+    public SimpleNormaliser(@Value("${normalisation.titles}") List<String> normalisedTitles) {
+        this.normalisedTitles = normalisedTitles;
+    }
+
     @Override
     public String normalise(String input) {
+        System.out.println(input);
+        normalisedTitles.forEach(System.out::println);
         return "";
     }
 }
