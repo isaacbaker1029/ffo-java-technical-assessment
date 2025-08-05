ca4b34a3a37ca692e86e53d0ee28a5f076a131ec
Isaac <isaac.l.baker@hotmail.com>
2025-08-05 11:11:38 +0100

    feat: cleaned up and made the main(run) look nicer

--- src/main/java/com/ffo/assessment/JobNormaliserApplication.java
+++ src/main/java/com/ffo/assessment/JobNormaliserApplication.java
@@ -2,6 +2,8 @@ package com.ffo.assessment;
 
 import com.ffo.assessment.service.NormalisationService;
 import com.ffo.assessment.service.impl.SimpleNormaliser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -9,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JobNormaliserApplication implements CommandLineRunner {
+    private static final Logger logger = LoggerFactory.getLogger(JobNormaliserApplication.class);
 
     @Autowired
     NormalisationService normalisationService;
@@ -21,14 +24,21 @@ public class JobNormaliserApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         // Using the sample code given
-        String jt = "Java engineer";
-        String normalisedTitle = normalisationService.normalise(jt);
-        System.out.println(normalisedTitle);
-        jt = "C# engineer";
-        normalisedTitle = normalisationService.normalise(jt);
-        System.out.println(normalisedTitle);
-        jt = "Chief Accountant";
-        normalisedTitle = normalisationService.normalise(jt);
-        System.out.println(normalisedTitle);
+        logger.info("--- Running Normalisation Service Demonstration ---");
+        logger.info("This demonstration executes the sample code provided in the technical specification.");
+
+        String jt1 = "Java engineer";
+        String normalisedTitle1 = normalisationService.normalise(jt1);
+        logger.info("Input: '{}' -> Output: '{}'", jt1, normalisedTitle1);
+
+        String jt2 = "C# engineer";
+        String normalisedTitle2 = normalisationService.normalise(jt2);
+        logger.info("Input: '{}' -> Output: '{}'", jt2, normalisedTitle2);
+
+        String jt3 = "Chief Accountant";
+        String normalisedTitle3 = normalisationService.normalise(jt3);
+        logger.info("Input: '{}' -> Output: '{}'", jt3, normalisedTitle3);
+
+        logger.info("--- Demonstration Complete ---");
     }
 }
